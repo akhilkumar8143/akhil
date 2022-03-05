f3adfaed4e0f267e599db5e596cf60bfe6661155
akhilkumar8143 <100994962+akhilkumar8143@users.noreply.github.com>
2022-03-05 15:10:16 +0530

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,59 @@
+#BANKING MADE SIMPLE FOR HNIs
+class dbsbank(object):
+    def__init__(self):
+        self.name=name
+        print("Hello!! Welcome to Digital bank")
+    def wm1(self):
+        self.name=A
+        self.mobnum=1234
+
+#login
+
+while (loop == 'true'):
+    username = input("Please enter your username: ")
+    if (username == CorrectUsername):
+    	password = input("Please enter your password: ")
+        if (password == CorrectPassword):
+            print("Logged in successfully as ") + username
+            loop = 'false'
+        else:
+            print ("Password incorrect!")
+    else:
+        print ("Username incorrect!")
+
+#logout
+def logout():
+    if session.pop('account', None):
+        print('Logout successfully!')
+        return redirect(url_for('login'))
+
+
+import datetime
+class HNI:
+
+    def __init__(self, name, surname, birthdate, address, telephone, email):
+        self.name = name
+        self.surname = surname
+        self.birthdate = birthdate
+
+        self.address = address
+        self.telephone = telephone
+        self.email = email
+person = Person(
+    "Jane",
+    "Doe",
+    datetime.date(1992, 3, 12), # year, month, day
+    "No. 12 Short Street, Greenville",
+    "555 456 0987",
+    "jane.doe@example.com"
+)
+
+wm=wm1()
+print("details of wm")
+
+
+
+
+
+        
+        
